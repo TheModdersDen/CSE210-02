3c06a0e63f05b5e0656a56cc063932954239ce5f
TheModdersDen <monsterspawned@gmail.com>
2022-01-20 20:44:39 -0700

    Testing 123

--- card_game.py
+++ card_game.py
@@ -10,6 +10,7 @@ class Card_Game():
         print(f"Welcome to {self.common_data.game_name}, {self.name}!")
 
     def start_game(self):
+        self.card1 = self.draw_card()
         if self.ask_question("Would you like to play game (Y/N)? "):
             print("Great! Let's get started!")
             sleep(0.5)
@@ -21,26 +22,33 @@ class Card_Game():
             print("Come again soon! :)")
             sleep(1)
             exit(0)
-        
 
     def run_game(self):
-        self.card1 = self.draw_card()
-        self.new_card = None
-        while True:
+        self.common_data.current_turn += 1
+        if self.commmon_data.current_turn == 1:
+            self.prev_card = self.card1
+        else:
+            self.prev_card = self.
+        self.next_card = None
+        self.prev_card = None
+        question = self.ask_question("Would you like to play game (Y/N)? ")     
+        while question != False:
+            self.prev_card =
             print(f"The current card is: {self.card1}")
-            guess = str(input("Is the next card higher or lower than the previous card [h/l]? "))
+            guess = input(int("Is the next card higher or lower than the previous card [h/l]? "))
             if guess in ["H".upper(), "HIGHER".upper()]:
                 self.new_card = self.draw_card()
                 if self.card1 > self.new_card:
                     print(self.common_data.get_encouragement())
-                else:
-                    print(self.common_data.get_wrong_choice())
             if guess in ["L".upper(), "LOWER".upper()]:    
                 self.new_card = self.draw_card()
                 if self.card1 < self.new_card:
                     print(self.common_data.get_encouragement())
-                else:
-                    print(self.common_data.get_wrong_choice())
+            else:
+                print(self.common_data)
+            self.common_data.current_turn += 1
+            question = self.ask_question("Would you like to play game (Y/N)? ") 
+
     def draw_card(self):
         card = Card()
         return card.card_num
