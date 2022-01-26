32123c26b5035b4a95c76fd007f5c8ee62c15504
joshliddi15 <92556139+joshliddi15@users.noreply.github.com>
2022-01-26 15:56:46 -0700

    Added handling for when the same card is drawn

--- game_common.py
+++ game_common.py
@@ -14,3 +14,7 @@ class Game_Common():
     def get_wrong_choice(self):
         return random.choice(["Not as lucky...", "Keep trying!", "What a loss!", "Getting there... but not quite there.", "Not the best guess.", "WOW...", "Not a winner, that one.",\
             "Target missed", "Negative on your last.", "Almost!", "No good!", "Unfortunate, really.", "Dissapointing... For you and the audience.", "You can do better than that!"])
+    
+    def get_tie_advice(self):
+        return random.choice(["Looks like this round is a tie", "No points this round", "That't the same as the last card", "At least you're consistent"])
+
